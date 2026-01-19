339c003e84ee5ab81a9347111957321c727dc933
Vanessa <240270@utxicotepec.edu.mx>
2026-01-19 15:48:45 -0600

    Práctica 02
    update: Se finalizo el código del Ejercicio 03: Funciones

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -37,7 +37,7 @@ function saludar()
 function fechaActual()
 {
     const fecha = new Date()  //Milisegundos
-    const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado"]
+    const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
     const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 
     const fecha_formatoMX = `${dias[fecha.getDay()]}, ${fecha.getDate()} de ${meses[fecha.getMonth()]} de ${fecha.getFullYear()}`;
@@ -48,7 +48,7 @@ function fechaActual()
 
 console.warn("3.- Funciones que retornan un valor, pero tienen parametros de entrada");
 let hoy = fechaActual();
-console.log(fechaActual);
+console.log(fechaActual());
 console.log(typeof(fechaActual));
 console.log(hoy);
 
@@ -103,3 +103,76 @@ console.log(`${loginStatus? "El usuario admin se ha logeado correctamente." : "H
 console.log("Test 4 - Usuario: Vanessa , password: abcd");
 loginStatus = login("Vanessa", "abcd");
 console.log(`${loginStatus? "El usuario admin se ha logeado correctamente." : "Hubo un error en el login del usuario admin."}`);
+
+
+// 5. Función anónima sin parámetros
+ isNewUser = function () {
+    const hoy = new Date();
+
+    return (
+        lastLogin.getFullYear() === hoy.getFullYear() &&
+        lastLogin.getMonth() === hoy.getMonth() &&
+        lastLogin.getDate() === hoy.getDate()
+    );
+};
+console.warn("5. Funciones Anónimas, sin parámetros");
+
+console.log("Test 1 - Fecha de ultimo acceso es igual a la fecha de hoy");
+console.log(`La fecha del último acceso es: ${lastLogin}`);
+console.log(`El usuario logeado es: ${isNewUser()?"Nuevo Usuario":"Usuario Antiguo"}`);
+
+console.log("--------------------------------------------------");
+lastLogin= new Date("2025/12/31");
+console.log("Test 2 - Fecha de ultimo acceso es diferente a la fecha de hoy");
+console.log(`La fecha del último acceso es: ${lastLogin}`);
+console.log(`El usuario logeado es: ${isNewUser()?"Nuevo Usuario":"Usuario Antiguo"}`);
+
+// 6. Funciones Anónimas con Parámetros  (Versión Arrow o Lambda)
+
+const sumar =  (a, b) =>{
+    let resultado= a+b;
+    return resultado;
+}
+
+console.warn("6. Funciones Anónima con Parámetros")
+console.log(`El resultado de la suma de 15 + 83 es: ${sumar(15,83)}`);
+
+/* Cuando la función anónima tiene solo una línea de operación se puede usar una versión simplificada que no usa {} llaves , ni la palabra reservada (return) */
+
+const multiplicar = (a,b) => a*b;
+console.log(`El resultado de la suma de 15 + 125 es: ${multiplicar(15,125)}`);
+
+// 7. Funciones Callback (Regreaso de llamado)
+console.warn("7. Funciones Anónimas Callback (Respuesta)")
+const recoverPassword = function(email, callback)
+{
+
+    // Generamos el código a enviar al usuario.
+    const recoveryCode= Math.floor(1000000 + Math.random()*900000)
+
+    console.log(`
+=======================================================================
+Solicitud de recuperación recibida
+Correo del usuario sollicitante: ${email}
+Generando Código de Recuperación...
+Código de Seguridad Generado: ${recoveryCode}
+Enviando el correo al usuario...
+Correo Enviado a : ${email}, con el código de seguridad: ${recoveryCode}
+=======================================================================`);
+
+
+// definiendo la respues del sistema
+const response ={
+    status: "OK",
+    message: "Código de recuperación enviado satisfactoriamente."
+};
+
+callback(response);
+};
+
+// Invocación de una función callback
+recoverPassword("vanessa@gmail.com", 
+    function(systemResponse){
+        console.log("Respuesta del sistema: ");
+        console.log(systemResponse.message);
+    });
